f9e6fa9652633114086236bf40a85141397b1dff
gandra70 <dragan@gmail.com>
2019-04-19 16:57:20 +0200

    'changes'

--- sp500.py
+++ sp500.py
@@ -7,6 +7,41 @@ from pandas_datareader.tests import yahoo
 # Ticker is used from yahoo finance. S&P 500 (^GSPC)
 startDate = datetime.datetime(2018, 1, 1)
 endDate = pd.datetime.now()
-df = data.DataReader(name="^GSPC", data_source="yahoo",start=startDate , end=endDate )
+df = data.DataReader(name="^GSPC", data_source="yahoo", start=startDate , end=endDate )
 
-print(df)
+plot = figure(x_axis_type='datetime', width=1000, height=300)
+
+def increaseDecrease(closePrice , openPrice):
+    if closePrice > openPrice:
+        value = "Increase"
+    elif closePrice < openPrice:
+        value = "Decrease"
+    else:
+        value = "Equal"
+    return value
+
+df["Status"] = [increaseDecrease(closePrice, openPrice) for closePrice, openPrice in zip(df.Close, df.Open)]
+df["Middle"] = (df.Open + df.Close)/2
+df["Height"] = abs(df.Close - df.Open)
+
+hours12 = 12 * 60 * 60 * 1000
+
+
+plot.rect(df.index[df.Status == "Increase"],
+          df.Middle[df.Status == "Increase"],
+          hours12,
+          df.Height[df.Status == "Increase"],
+          fill_color = "#e01414",
+          line_color = "black"
+          )
+
+plot.rect(df.index[df.Status == "Decrease"],
+          df.Middle[df.Status == "Decrease"],
+          hours12,
+          df.Height[df.Status == "Decrease"],
+          fill_color = "#142be0",
+          line_color = "black"
+          )
+
+output_file('gspcGraph.html')
+show(plot)
