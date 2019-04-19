c7d8a81204c887ee6f4f30d6f696e5a38180339a
gandra70 <dragan@gmail.com>
2019-04-19 16:09:52 +0200

    'changes'

---
+++ gold.py
@@ -0,0 +1,12 @@
+import datetime
+import pandas as pd
+from bokeh.plotting import figure, output_file, show
+from pandas_datareader import data
+from pandas_datareader.tests import yahoo
+
+# Ticker is used from yahoo finance. Gold Jun 19 (GC=F)
+startDate = datetime.datetime(2018, 1, 1)
+endDate = pd.datetime.now()
+df = data.DataReader(name="^GSPC", data_source="yahoo",start=startDate , end=endDate )
+
+print(df)
